3c4f5f78c8e9fbe20a163f9ce9a566454f930a7e
lutzer <in_the_fishtank@gmx.de>
2017-07-23 11:24:28 +0200

    changed lib

--- code/signalController/src/main.cpp
+++ code/signalController/src/main.cpp
@@ -9,57 +9,42 @@
  * Use freely
  */
 
- #include <Encoder.h>
- //#include <AD9850.h>
+#include <Encoder.h>
+#include <DDS.h>
 
- #define W_CLK 8       // Pin 8 - connect to AD9850 module word load clock pin (CLK)
- #define FQ_UD 9       // Pin 9 - connect to freq update pin (FQ)
- #define DATA 10       // Pin 10 - connect to serial data load pin (DATA)
- #define RESET 11      // Pin 11 - connect to reset pin (RST).
+#define W_CLK 8       // Pin 8 - connect to AD9850 module word load clock pin (CLK)
+#define FQ_UD 9       // Pin 9 - connect to freq update pin (FQ)
+#define DATA 10       // Pin 10 - connect to serial data load pin (DATA)
+#define RESET 11      // Pin 11 - connect to reset pin (RST).
 
- #define ENCODER_MAX_STEPS 600
-
- #define pulseHigh(pin) {digitalWrite(pin, HIGH); digitalWrite(pin, LOW); }
+#define ENCODER_MAX_STEPS 600
 
 Encoder encoder1(3, 4);
 Encoder encoder2(5, 6);
 long enc1_oldPosition = 0;
 long enc2_oldPosition = 0;
 
- // transfers a byte, a bit at a time, LSB first to the 9850 via serial DATA line
-void tfr_byte(byte data)
-{
-  for (int i=0; i<8; i++, data>>=1) {
-    digitalWrite(DATA, data & 0x01);
-    pulseHigh(W_CLK);   //after each bit sent, CLK is pulsed high
-  }
-}
+DDS dds(W_CLK, FQ_UD, DATA, RESET);
 
- // frequency calc from datasheet page 8 = <sys clock> * <frequency tuning word>/2^32
-void sendFrequency(double frequency) {
-  int32_t freq = frequency * 4294967295/125000000;  // note 125 MHz clock on 9850
-  for (int b=0; b<4; b++, freq>>=8) {
-    tfr_byte(freq & 0xFF);
-  }
-  tfr_byte(0x000);   // Final control byte, all 0 for 9850 chip
-  pulseHigh(FQ_UD);  // Done!  Should see output
+// exponential growing function
+double calculateFrequency(long enc1_pos, long enc2_pos) {
+  double mainFreq = exp((float)enc1_pos / ENCODER_MAX_STEPS * 6.8);
+  double fineFreq = mainFreq * 0.1 * enc2_pos / ENCODER_MAX_STEPS;
+  return mainFreq + fineFreq;
 }
 
 void setup() {
- // configure arduino data pins for output
-  pinMode(FQ_UD, OUTPUT);
-  pinMode(W_CLK, OUTPUT);
-  pinMode(DATA, OUTPUT);
-  pinMode(RESET, OUTPUT);
-
-  pulseHigh(RESET);
-  pulseHigh(W_CLK);
-  pulseHigh(FQ_UD);  // this pulse enables serial mode - Datasheet page 12 figure 10
+  dds.init();
 
   Serial.begin(9600);
 
-  encoder1.write(ENCODER_MAX_STEPS * 0.9);
-  encoder2.write(0);
+  enc1_oldPosition = ENCODER_MAX_STEPS * 0.9;
+  enc2_oldPosition = 0;
+
+  encoder1.write(enc1_oldPosition);
+  encoder2.write(enc2_oldPosition);
+
+  dds.setFrequency(calculateFrequency(enc1_oldPosition, enc2_oldPosition));
 
   Serial.println("started");
 }
@@ -68,23 +53,17 @@ void loop() {
   long enc1_newPosition = encoder1.read();
   long enc2_newPosition = encoder2.read();
   if (enc1_newPosition != enc1_oldPosition || enc2_newPosition != enc2_oldPosition) {
+
     enc1_oldPosition = constrain(enc1_newPosition, 0, ENCODER_MAX_STEPS);
     encoder1.write(enc1_oldPosition);
-    // if (enc1_newPosition <= 0) {
-    //   enc1_oldPosition = 0;
-    //   encoder2.write(0);
-    //   encoder1.write(enc1_oldPosition);
-    // } else if (enc1_newPosition > ENCODER_MAX_STEPS) {
-    //   enc1_oldPosition = ENCODER_MAX_STEPS;
-    //   encoder2.write(0);
-    // }
+
     enc2_oldPosition = enc2_newPosition;
-      // range from e^0 to e^8.0 (3000 hz), so make that the maximum
-    double mainFreq = exp((float)enc1_newPosition / ENCODER_MAX_STEPS * 6.8);
-    double fineFreq = mainFreq * 0.1 * enc2_newPosition / ENCODER_MAX_STEPS;
-    double frequency =  mainFreq + fineFreq;
+
+    double frequency = calculateFrequency(enc1_newPosition, enc2_newPosition);
+
+    dds.setFrequency(frequency);
+    
+    Serial.print("f1:");
     Serial.println(frequency);
-    sendFrequency(frequency);
   }
-    // freq
 }
